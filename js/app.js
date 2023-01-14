6f04937eb706712f05c7c87d31132aa1cac35687
ohadk <ohadkachlon@gmail.com>
2023-01-14 21:13:04 +0200

    start

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
-$(".default_option").click(function(){
-    $(".dropdown ul").addClass("active");
-  });
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
   
-  $(".dropdown ul li").click(function(){
-    var text = $(this).text();
-    $(".default_option").text(text);
-    $(".dropdown ul").removeClass("active");
-  });
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
